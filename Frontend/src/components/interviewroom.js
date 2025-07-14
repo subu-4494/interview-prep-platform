a9ca073c8bf24a425df21b6c57b5d14bf98aa6f5
Sourabh <sourabroy2018@gmail.com>
2025-07-15 04:34:41 +0530

    added backend link

--- Frontend/src/components/interviewroom.js
+++ Frontend/src/components/interviewroom.js
@@ -24,7 +24,7 @@ const InterviewRoom = ({ roomId }) => {
   };
 
   useEffect(() => {
-    const socket = io("http://localhost:5000");
+    const socket = io("https://interview-prep-platform-07wl.onrender.com");
     socketRef.current = socket;
 
     navigator.mediaDevices
